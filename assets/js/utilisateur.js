affda05b65e77621e9fec245cbcec9c691c46c4c
micpiwo38 <michel.michael38@gmail.com>
2026-03-10 09:09:11 +0100

    Modif a partir de la barnce inscription

--- assets/js/utilisateur.js
+++ assets/js/utilisateur.js
@@ -3,3 +3,9 @@ console.log("COnnexion et Inscription utilisateur");
 console.log("la branche connexion");
 
 const user = document.getElementById("user");
+
+const register_user = () => {
+  console.log("Inscrire un utilisateur");
+};
+
+register_user();
